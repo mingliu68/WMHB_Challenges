bc8e02ea1ad8069269738a7a0eedbfcb12ed7cbc
Ming Liu <mliu@Mings-Mac-mini.fios-router.home>
2023-05-04 18:43:22 -0400

    week 10

--- src/WhiteBoarding_w10/BinaryInt.java
+++ src/WhiteBoarding_w10/BinaryInt.java
@@ -4,6 +4,48 @@ package WhiteBoarding_w10;
 
 // e.g. input(17), Output: 10001
 
+// base 2 numerical
+
+
 public class BinaryInt {
-    
+     
+    public static void main(String[] args) {
+        System.out.println(getBinary(17));
+        System.out.println(getBinary(18));
+        System.out.println(getBinary(19));
+        System.out.println(getBinary(68));
+
+    }
+
+    public static String getBinary(int n) {
+
+        int power = 0;
+
+        while(true) {
+            if((int)Math.pow(2, power) > n)  {
+                power--;
+                break;
+            }
+            power++;
+        }
+
+        return helper(n, power);
+        
+    }
+
+    public static String helper(int n, int power) {
+        if(power == 0) return "";
+        if (n == 0) return "";
+        if (n == 1) return "1";
+        if (n == 2) return "10";
+        if (n == 3) return "11";
+
+        int remainder = (int)Math.pow(2, power) <= n ? n - (int)Math.pow(2, power) : n;
+
+        String base = (int)Math.pow(2, power) <= n ? "1" + "0".repeat(power) : "";
+        String recurBase = helper(remainder, power-1);
+        
+        return !base.equals("") ? base.substring(0, base.length() - recurBase.length()) + recurBase : recurBase;
+
+    }    
 }
--- src/WhiteBoarding_w10/NumberPalindrome.java
+++ src/WhiteBoarding_w10/NumberPalindrome.java
@@ -4,5 +4,37 @@ package WhiteBoarding_w10;
 // Constraint: Do not convert from int to String. Instead use mathematical manipulation.
 
 public class NumberPalindrome {
-    
+    public static void main(String[] args) {
+
+        System.out.println(isPalindrome(1234));
+        System.out.println(isPalindrome(654321));
+        System.out.println(isPalindrome(54321));
+        System.out.println(isPalindrome(31213));
+        System.out.println(isPalindrome(156521));
+
+
+
+
+    }
+
+    public static Boolean isPalindrome(int n) {
+        if(n / 10 == 0) return true;
+
+        int length = 0;
+        int counter = 1; 
+        while(counter <= n) {
+            length ++;
+            counter *= 10;
+        }
+        counter /= 10;
+        
+        if((n / counter) == (n % 10)) {
+
+            n = n % counter;
+            n /= 10;
+            return isPalindrome(n);
+        } else {
+            return false;
+        }
+    }
 }
