3bd87e2ef84bdff9f4c032cfb4caaf8ec5ddf264
Ming Liu <mliu@Mings-Mac-mini.fios-router.home>
2023-05-05 02:16:16 -0400

    sat challenge 3

--- src/SatChallenge_3/ExpressionToInteger.java
+++ src/SatChallenge_3/ExpressionToInteger.java
@@ -1,5 +1,9 @@
 package SatChallenge_3;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 // You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
 
 // Evaluate the expression. Return an integer that represents the value of the expression.
@@ -23,5 +27,52 @@ package SatChallenge_3;
 // Explanation: (4 + (13 / 5)) = 6
 
 public class ExpressionToInteger {
-    
+    public static void main(String[] args) {
+        ArrayList<String> arr1 = new ArrayList<>(Arrays.asList("2","1","+","3",""));
+        ArrayList<String> arr2 = new ArrayList<>(Arrays.asList("4","13","5","/","+"));
+
+        System.out.println(returnInt(arr1));
+        System.out.println(returnInt(arr2));
+    }
+
+    public static Integer returnInt(ArrayList<String> arr) {
+        // going from left to right, look for operators and the two elements a head of it
+        // removing the 3 elements and replace with single element which is the result
+        // recursively run the array until we are left with just single element
+
+        if(arr.size() == 1) {
+            return Integer.parseInt(arr.get(0));
+        }
+        List<String> operators = Arrays.asList(new String[]{"+", "-", "/", ""});
+       
+        
+        for(int i = 0; i < arr.size(); i++) {
+
+            if(operators.contains(arr.get(i))) {
+                String result = operation(Integer.parseInt(arr.get(i-2)), Integer.parseInt(arr.get(i-1)), arr.get(i));
+                arr.set(i-2, result);
+                arr.remove(i);
+                arr.remove(i-1);
+                break;
+            }
+            
+        }
+
+        return returnInt(arr);
+    }
+
+    public static String operation(Integer a, Integer b, String operator) {
+        switch(operator) {
+            case "+":
+                return Integer.toString(a + b);
+            case "-":
+                return Integer.toString(a - b);
+            case "/":
+                return Integer.toString(a / b);
+            case "":
+                return Integer.toString(a * b);
+            default:
+                return null;
+        }
+    }
 }
