c7a6c5e8ab24a07dd101993b4147559f931fb23b
Ming Liu <mliu@Mings-Mac-mini.fios-router.home>
2023-05-07 16:55:34 -0400

    wb wk_6

--- src/WhiteBoarding_w6/MaximizingProfit.java
+++ src/WhiteBoarding_w6/MaximizingProfit.java
@@ -2,9 +2,11 @@ package WhiteBoarding_w6;
 
 // You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
-// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a 
+// different day in the future to sell that stock.
 
-// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+// Return the maximum profit you can achieve from this transaction. 
+// If you cannot achieve any profit, return 0.
 
 // Example 1:
 // Input: prices = [7,1,5,3,6,4]
@@ -22,5 +24,39 @@ package WhiteBoarding_w6;
 //     0 <= prices[i] <= 104
 
 public class MaximizingProfit {
-    
+    public static void main(String[] args) {
+        int[] prices1 = {7,1,5,3,6,4};
+        int[] prices2 = {7,6,4,3,1};
+        int[] prices3 = {7,6,4,3,5,17,9,3,26,12,14};
+
+        System.out.println(maximizingProfit(prices1));
+        System.out.println(maximizingProfit(prices2));
+        System.out.println(maximizingProfit(prices3));
+    }
+
+    public static int maximizingProfit(int[] prices) {
+        if(prices.length == 1) return 0;
+        if(prices.length == 2) {
+            return prices[1] - prices[0] > 0 ? prices[1] - prices[0] : 0;
+        }
+
+        //code
+        int left = 0;
+        int right = 1;
+        int max = 0;
+
+        for(int i = 0; i < prices.length; i++) {
+            while((left < prices.length - 1) && (right < prices.length) && (left < right)) {
+                if (prices[left] > prices[right]) {
+                    left++;
+                    if(left == right) right++;
+                } else {
+                    max = prices[right] - prices[left] > max ? prices[right] - prices[left] : max;
+                    right++;
+                } 
+            }
+        }
+
+        return max;
+    }
 }
--- src/WhiteBoarding_w6/SumOfLowPoints.java
+++ src/WhiteBoarding_w6/SumOfLowPoints.java
@@ -1,19 +1,46 @@
 package WhiteBoarding_w6;
 
+import java.rmi.StubNotFoundException;
+
 // Given a 2-dimensional array of numbers (i.e. an array of arrays), write a function
 //   that returns the sum of all numbers that are *low points*. A low point is any
 //   number that is smaller than its neighbors above, below, and to the left and right.
 //   Example:
 
 //  Int[][] a ={    {9, 8, 6, 5, 4},    
-//         {7,`4`,7, 7,`2`},    
-//         {6, 5,`0`,5, 6},
-//         {3,`1`,8, 5, 8}}
+//                  {7,`4`,7, 7,`2`},    
+//                  {6, 5,`0`,5, 6},
+//                  {3,`1`,8, 5, 8}}
 
 //  The ‘ ‘ numbers are low points, so the function should return 4 + 2 + 0 + 1 = 7.
 
 // Hint: The trick to this problem is making sure indices do not go out of bounds.
 
 public class SumOfLowPoints {
-    
+    public static void main(String[] args) {
+        int[][] a = {
+                    {9,8,6,5,4},    
+                    {7,4,7,7,2},    
+                    {6,5,0,5,6},
+                    {3,1,8,5,8}};
+        System.out.println(getSumOfLowPoints(a));
+    }
+    public static int getSumOfLowPoints(int[][] matrix) {
+        int result = 0;
+        boolean top;
+        boolean bottom;
+        boolean left;
+        boolean right;
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                top = i-1 < 0 ? true : matrix[i-1][j] > matrix[i][j] ? true : false;
+                bottom = i+1 >= matrix.length ? true : matrix[i+1][j] > matrix[i][j] ? true : false;
+                left = j-1 < 0 ? true : matrix[i][j-1] > matrix[i][j] ? true : false;
+                right = j+1 >= matrix[0].length ? true : matrix[i][j+1] > matrix[i][j] ? true : false;
+                
+                result = top && bottom && left && right ? result + matrix[i][j] : result;
+            }
+        }
+        return result;
+    }
 }
