f499940e439f89e25cefa4f8604cdfe98e590098
Ming Liu <mliu@Mings-Mac-mini.local>
2023-06-04 18:52:15 -0400

    single occurance int

--- src/WhiteBoarding_w15/SingleOccuranceInteger.java
+++ src/WhiteBoarding_w15/SingleOccuranceInteger.java
@@ -1,5 +1,7 @@
 package WhiteBoarding_w15;
 
+import java.util.HashMap;
+
 /*
  * Given an array of integers A, return the largest integer that only occurs once.
 
@@ -22,5 +24,34 @@ Note:
 0 <= A[i] <= 1000
  */
 public class SingleOccuranceInteger {
-    
+    public static void main(String[] args) {
+        int[] nums1 = {5,7,3,9,4,9,8,3,1};
+        int[] nums2 = {9,9,8,8};
+
+        System.out.println(getMaxOnce(nums1));
+        System.out.println(getMaxOnce(nums2));
+    }
+
+    public static int getMaxOnce(int[] arr) {
+        int[] max = new int[1];
+        max[0] = -1;
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        for(int num : arr) {
+            if(map.containsKey(num)) {
+                map.put(num, false);
+            } else {
+                map.put(num, true);
+            }
+        }
+        
+        map.forEach((key, value) -> {
+            if(value && key > max[0]) setterHelper(key, max); 
+        });
+
+        return max[0];
+    }
+
+    private static void setterHelper(int newMax, int[] max) {
+        max[0] = newMax;
+    }   
 }
